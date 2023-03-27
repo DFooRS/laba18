9d3cb40890550497d17ed77b0d718b26e9330b40
DFooRS <dima.lutsenko03@gmail.com>
2023-03-27 13:22:19 +0300

    2ind edited

--- 2ind.py
+++ 2ind.py
@@ -8,15 +8,17 @@ if __name__ == "__main__":
     cmd = input(': ')
     file_list = []
 
-    if 'cat' in cmd:
-        k = cmd.find(' ')
-        cmd = cmd[k+1:]
-        file_list = cmd.split()
-        print(file_list)
-        for i, item in enumerate(file_list):
-            with open(item, "r", encoding="utf-8") as fileptr:
-                text = str(fileptr.readlines())
-                print(text)
-
+    if cmd.find("cat") == 0:
+        if len(cmd) > 3:
+            k = cmd.find(' ')
+            cmd = cmd[k+1:]
+            file_list = cmd.split()
+            print(file_list)
+            for i, item in enumerate(file_list):
+                with open(item, "r", encoding="utf-8") as fileptr:
+                    text = str(fileptr.readlines())
+                    print(text)
+        else:
+            print("Команда введена без аргументов", file=sys.stderr)
     else:
         print("Команда введена неверно", file=sys.stderr)
