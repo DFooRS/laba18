2a3bc6da073db139ff685be8c6987e7c157c19d3
DFooRS <dima.lutsenko03@gmail.com>
2023-03-27 13:13:20 +0300

    1ind.py edited

--- 1ind.py
+++ 1ind.py
@@ -6,9 +6,13 @@ if __name__ == "__main__":
     with open("TextInd1.txt", "r", encoding="utf-8") as fileptr:
         sentences = str(fileptr.readlines())
         sentences = sentences[2:(len(sentences)-2)]
+        other_sent = ""
 
         while len(sentences) > 1:
             n = sentences.find('.')
             if sentences[1] == ' ':
                 print(sentences[:n+1])
+            else:
+                other_sent += sentences[:n+2]
             sentences = sentences[n+2:]
+        print(other_sent)
